ecce95c530f3c2ebb2293a7e3e0780048f3075df
Aanyaaaaaa <aanya.gupta2022@vitstudent.ac.in>
2025-04-18 02:30:54 +0530

    Initial commit for space explorers project

--- client/src/containers/HomepageContainer.js
+++ client/src/containers/HomepageContainer.js
@@ -5,6 +5,7 @@ import Leaderboard from '../components/Leaderboard';
 import PlayerService from '../service/PlayerService';
 import styled from 'styled-components';
 import NasaAPIkey from './../config.js';
+import config from './../config.js'; // Assuming config.js is located at src/config.js
 import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
